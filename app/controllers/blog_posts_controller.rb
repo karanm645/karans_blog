780cd65910d01579a2bf3d800dce69c8cb6ef6d4
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2024-01-20 15:52:39 -0600

    Change index for user

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -3,7 +3,7 @@ class BlogPostsController < ApplicationController
   before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
 
   def index 
-    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.sorted
+    @blog_posts = user_signed_in? ? BlogPost.all : BlogPost.sorted
   end 
 
   def show 
